42c060ae7263317ec14f06d34b363177a78ea4cc
Geoffrey Omondi Odiwour <GOdiwour@safaricom.co.ke>
2025-06-08 02:15:14 +0300

    Initial commit with Sanity Setup

---
+++ sanity.config.ts
@@ -0,0 +1,24 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'zenith-portfolio',
+
+  projectId: 'i8m1stf5',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+
+  updates: {
+    auto: false
+  },
+})
+
+
